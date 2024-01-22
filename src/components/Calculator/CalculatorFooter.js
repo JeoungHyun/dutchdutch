861f678fee8b0fe61439d651b792b9a1d6d75960
JeoungHyun <cho3153238@naver.com>
2024-01-22 19:12:25 +0900

    gh-pages 설치

--- src/components/Calculator/CalculatorFooter.js
+++ src/components/Calculator/CalculatorFooter.js
@@ -4,7 +4,7 @@ import { Box, Button } from "@mui/material";
 const CalculatorFooter = () => {
   return (
     <Box sx={{ cursor:'pointer',color:'white',fontSize:'15px',backgroundColor: "#4f515af7", height: "40px",display:'flex',alignItems:'center',justifyContent:'center' }}>
-      정산 하기!
+      정산 하기!!!
     </Box>
   );
 };
